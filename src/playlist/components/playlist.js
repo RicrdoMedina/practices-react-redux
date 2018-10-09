72d7bb736d0aefd89d68b2e41a377a8be97dad1b
Ricardo Medina <rcrdmedina6@gmail.com>
2018-10-08 20:22:11 -0430

    Composición de componentes

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Media from './media'
 import './playlist.css'
+import Play from '../../icons/components/play'
 
 export default (props) => {
   console.log(props)
@@ -10,6 +11,7 @@ export default (props) => {
     <div className="Playlist">
       <h3 className = "Playlist-descripcion">{description}</h3>
 			<h1 className = "Playlist-titulo">{title}</h1>
+      <Play color="red" size="32"/>
       {
         playlist.map((item) => {
           return <Media {...item} key={item.id}/>
