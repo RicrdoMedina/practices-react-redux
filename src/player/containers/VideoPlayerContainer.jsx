fee7ce8739a6cb8c323497cd5de467056e3f8c2a
Ricardo Medina <rcrdmedina6@gmail.com>
2018-10-10 16:56:26 -0430

    Barra de progreso y spinner

--- src/player/containers/VideoPlayerContainer.jsx
+++ src/player/containers/VideoPlayerContainer.jsx
@@ -6,12 +6,18 @@ import PlayPause from '../components/PlayPause.jsx'
 import Timer from '../components/Timer.jsx'
 import Controls from '../components/videoPlayerControls.jsx'
 import { formattedTime } from '../../utils/libs'
+import ProgressBar from '../components/progressBar.jsx'
 
 class VideoPlayer extends Component {
   state = {
-    pause: true,
+    pause:  true,
     duration: 0,
-    currentTime: 0
+    currentTime: 0,
+    durationFloat: 0,
+    timeFloat: 0,
+    loading: true,
+    volume: 1,
+    lastValue: null,
   }
   togglePlay = (event) => {
     //actualizar el estado con un arrow function o una funcion normal para evitar posibles inconvenientes
@@ -32,14 +38,19 @@ class VideoPlayer extends Component {
   handleLoadedMetadata = event => {
     this.video = event.target
     this.setState({
-      duration: formattedTime(this.video.duration)
+      duration: formattedTime(this.video.duration),
+      durationFloat: this.video.duration
     })
   }
   handleTimeUpdate = event => {
     console.log(this.video.currentTime)
     this.setState({
-      currentTime: formattedTime(this.video.currentTime)
+      currentTime: formattedTime(this.video.currentTime),
+      timeFloat: this.video.currentTime
     })
+  }
+  handleProgressChange = event => {
+    this.video.currentTime = event.target.value
   }
   render () {
     return (
@@ -55,6 +66,11 @@ class VideoPlayer extends Component {
           <Timer
             duration={this.state.duration}
             currentTime={this.state.currentTime}
+          />
+          <ProgressBar
+            duration = { this.state.durationFloat }
+            value = { this.state.timeFloat }
+            handleProgressChange={this.handleProgressChange}
           />
         </Controls>
         <Video
