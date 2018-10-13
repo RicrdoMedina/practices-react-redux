c3582bc0fc278e5c8de211f5774c256b5bdb27e1
Ricardo Medina <rcrdmedina6@gmail.com>
2018-10-13 10:43:28 -0430

    Agregando un Reducer para manejar los datos

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import reducer from '../reducers/data'
 import Home from '../pages/containers/Home.jsx'
 import data from '../api.json'
 
@@ -12,7 +13,7 @@ const initialState = {
 }
 
 const store = createStore(
-                (state) => state,
+                reducer,
                 initialState,
                 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
               )
