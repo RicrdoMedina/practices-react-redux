91c356f3b3fb8946b928670dc9b976a51083341b
Ricardo Medina <rcrdmedina6@gmail.com>
2018-10-13 17:47:38 -0430

    Normalizando datos

--- src/entries/home.js
+++ src/entries/home.js
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux'
 import reducer from '../reducers/data'
 import Home from '../pages/containers/Home.jsx'
 import data from '../api.json'
+import normalizedData from '../schemas/index.js'
+
+console.log(normalizedData)
 
 const initialState = {
   data: {
